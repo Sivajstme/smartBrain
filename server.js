b59264aa572680f68e4c4b35bcc4625f9dbe4068
siva <shivajstme@gmail.com>
2020-01-27 12:50:04 +1100

    Database access

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const signIn = require('./controllers/signIn')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'sivasunkara',
-        password: '',
-        database: 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl:true
     }
 });
 
